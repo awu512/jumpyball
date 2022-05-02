418d17381fe5b5c4b3cee7b7bbe2a1d206c7c6c3
aidan <aid1@bamba.us>
2022-05-01 20:02:19 -0700

    clean up + switch to test level

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,13 @@
 #![allow(dead_code)]
-
-use frenderer::animation::{AnimationState};
-use frenderer::assets::AnimRef;
 use frenderer::camera::Camera;
 use frenderer::types::*;
 use frenderer::{Engine, Key, Result, WindowSettings};
 use std::rc::Rc;
 
-const DT: f64 = 1.0 / 60.0;
-const CIRC: f32 = 2. / PI;
-const PLAYER_R: f32 = 1.0;
-
-struct GameObject {
-    trf: Similarity3,
-    model: Rc<frenderer::renderer::skinned::Model>,
-    animation: AnimRef,
-    state: AnimationState,
-}
-impl GameObject {
-    fn tick_animation(&mut self) {
-        self.state.tick(DT);
-    }
-}
+// GAME SETTINGS
+const DT: f64 = 1.0 / 60.0; // time step
+const PR: f32 = 1.0; // sphere radius
+const PV: f32 = 0.2; // player movement velocity
 
 struct BoundingBox {
     min_x: f32,
@@ -43,14 +29,6 @@ impl BoundingBox {
             max_z
         }
     }
-
-    fn center(self) -> Vec3 {
-        Vec3::new(
-            (self.max_x - self.min_x) / 2.,
-            (self.max_y - self.min_y) / 2.,
-            (self.max_z - self.min_z) / 2.,
-        )
-    }
 }
 
 struct Sphere {
@@ -58,45 +36,10 @@ struct Sphere {
     r: f32,
 }
 
-struct Plane {
-    // A normal, has to be a unit vector
-    n: Vec3,
-    // And a distance of how far along the normal this is
-    d: f32,
-}
-
-fn sphere_in_box(p: &Player, b: &BoundingBox) -> bool {
-    // Find the distance of the sphere's center to the plane
-    let s: Sphere = Sphere { 
-        pos: p.trf.translation + Vec3::new(0.32, 0.32, 0.32),
-        r: 0.32,
-    };
-
-    let offsets: Vec<Vec3> = vec![
-        Vec3::new(s.r, 0., 0.),
-        Vec3::new(-s.r, 0., 0.),
-        Vec3::new(0., s.r, 0.),
-        Vec3::new(0., -s.r, 0.),
-        Vec3::new(0., 0., s.r),
-        Vec3::new(0., 0., -s.r),
-    ];
-
-    for o in offsets {
-        let p: Vec3 = s.pos + o;
-        if p.x >= b.min_x && p.x <= b.max_x &&
-            p.y >= b.min_y && p.y <= b.max_y &&
-            p.z >= b.min_z && p.z <= b.max_z
-        {
-            return true;
-        }
-    }
-    false
-}
-
 fn handle_collision(p: &mut Player, b: &BoundingBox) {
     let s: Sphere = Sphere { 
-        pos: p.trf.translation + Vec3::new(0.32, 0.32, 0.32),
-        r: 0.32,
+        pos: p.trf.translation,
+        r: PR,
     };
 
     let closest = Vec3::new(
@@ -133,13 +76,8 @@ struct Player {
 
 struct Level {
     trf: Similarity3,
-    model: Rc<frenderer::renderer::textured::Model>,
-}
-struct Sprite {
-    trf: Isometry3,
-    tex: frenderer::assets::TextureRef,
-    cel: Rect,
-    size: Vec2,
+    // model: Rc<frenderer::renderer::textured::Model>,
+    model: Rc<frenderer::renderer::flat::Model>,
 }
 struct World {
     camera: Camera,
@@ -151,31 +89,32 @@ struct Flat {
     trf: Similarity3,
     model: Rc<frenderer::renderer::flat::Model>,
 }
+
 impl frenderer::World for World {
     fn update(&mut self, input: &frenderer::Input, _assets: &mut frenderer::assets::Assets) {
         let mut dc: Vec3 = self.player.trf.translation;
 
         // X MOVEMENT
         if input.is_key_down(Key::Right) {
-            self.player.v.x = 0.1
+            self.player.v.x = PV
         } else if input.is_key_down(Key::Left) {
-            self.player.v.x = -0.1
+            self.player.v.x = -PV
         } else {
             self.player.v.x = 0.
         };
 
         // Z MOVEMENT
         if input.is_key_down(Key::Down) {
-            self.player.v.z = 0.1
+            self.player.v.z = PV
         } else if input.is_key_down(Key::Up) {
-            self.player.v.z = -0.1
+            self.player.v.z = -PV
         } else {
             self.player.v.z = 0.
         };
 
         // JUMP MECHANICS
         if input.is_key_pressed(Key::Space) && self.player.jump_count < 2 {
-            self.player.v.y = 0.5;
+            self.player.v.y = 3. * PV;
             self.player.jump_count += 1;
         }
 
@@ -184,8 +123,8 @@ impl frenderer::World for World {
         self.player.trf.append_translation(self.player.v);
 
         // GROUND CHECK
-        if self.player.trf.translation.y < 0.32 {
-            self.player.trf.translation.y = 0.32;
+        if self.player.trf.translation.y < PR {
+            self.player.trf.translation.y = PR;
             self.player.jump_count = 0;
         }
 
@@ -202,9 +141,9 @@ impl frenderer::World for World {
         self.player.trf.prepend_rotation(Rotor3 {
             s: 1.,
             bv: Bivec3 {
-                xy: (self.player.v.x / CIRC) * rot_mult,
+                xy: ((self.player.v.x / (2. * PR)) / 2. * PI) * rot_mult,
                 xz: 0.,
-                yz: (-self.player.v.z / CIRC) * rot_mult,
+                yz: -((self.player.v.z / (2. * PR)) / 2. * PI) * rot_mult,
             },
         });
 
@@ -227,7 +166,8 @@ impl frenderer::World for World {
 
         rs.render_textured(self.player.model.clone(), self.player.trf, 0);
 
-        rs.render_textured(self.level.model.clone(), self.level.trf, 1);
+        rs.render_flat(self.level.model.clone(), self.level.trf, 1);
+        // rs.render_textured(self.level.model.clone(), self.level.trf, 1);
     }
 }
 fn main() -> Result<()> {
@@ -242,32 +182,42 @@ fn main() -> Result<()> {
     );
 
     let player_tex = engine.load_texture(std::path::Path::new("content/sphere_test_spiral.png"))?;
-    let player_mesh = engine.load_textured(std::path::Path::new("content/sphere_test.obj"))?;
+    let player_mesh = engine.load_textured(std::path::Path::new("content/test.obj"))?;
     let player_model = engine.create_textured_model(player_mesh, vec![player_tex]);
 
-    let level_tex = engine.load_texture(std::path::Path::new("content/level_1.png"))?;
-    let level_mesh = engine.load_textured(std::path::Path::new("content/level_1.obj"))?;
-    let level_model = engine.create_textured_model(level_mesh, vec![level_tex, level_tex]);
+    // let level_tex = engine.load_texture(std::path::Path::new("content/level_1.png"))?;
+    // let level_mesh = engine.load_textured(std::path::Path::new("content/level_1.obj"))?;
+    // let level_model = engine.create_textured_model(level_mesh, vec![level_tex, level_tex]);
+
+    let level_model = engine.load_flat(std::path::Path::new("content/untitled.obj"));
+
+    // let bounding_boxes = vec![
+    //     // BoundingBox::new(25.076244354248047, 25.076244354248047, 6.903861999511719, 6.903861999511719, 1.0054539442062378, 1.0054539442062378),
+    //     // BoundingBox::new(24.358890533447266, 24.358890533447266, 4.958309173583984, 4.958309173583984, -22.925790786743164, -22.925790786743164),
+    //     BoundingBox::new(1.5   , 2.5   , 0. , 3. , 1.    , 3.    ),
+    //     BoundingBox::new(-3.   , -2.   , 0. , 3. , -4.75 , -3.75 ),
+    //     BoundingBox::new(-5.25 , -4.25 , 0. , 3. , -4.75 , -3.75 ),
+    //     BoundingBox::new(-0.75 , 0.25  , 0. , 3. , -4.75 , -3.75 ),
+    //     BoundingBox::new(-6.   , -5.   , 0. , 3. , 4.    , 5.    ),
+    //     BoundingBox::new(-3.5  , -2.5  , 0. , 3. , 4.    , 5.    ),
+    //     BoundingBox::new(1.5   , 2.5   , 0. , 3. , -4.75 , -3.75 ),
+    //     BoundingBox::new(1.5   , 2.5   , 0. , 3. , -3.   , -1.   ),
+    //     BoundingBox::new(-1.   , 0.0   , 0. , 3. , 4.    , 5.    ),
+    //     // BoundingBox::new(-1.0006559021421708, 0.9993443362764083, -1.255562663078308, 4.24443781375885, 0.9995791912078857, 2.999579429626465)
+    // ];
 
     let bounding_boxes = vec![
-        // BoundingBox::new(25.076244354248047, 25.076244354248047, 6.903861999511719, 6.903861999511719, 1.0054539442062378, 1.0054539442062378),
-        // BoundingBox::new(24.358890533447266, 24.358890533447266, 4.958309173583984, 4.958309173583984, -22.925790786743164, -22.925790786743164),
-        BoundingBox::new(1.5   , 2.5   , 0. , 3. , 1.    , 3.    ),
-        BoundingBox::new(-3.   , -2.   , 0. , 3. , -4.75 , -3.75 ),
-        BoundingBox::new(-5.25 , -4.25 , 0. , 3. , -4.75 , -3.75 ),
-        BoundingBox::new(-0.75 , 0.25  , 0. , 3. , -4.75 , -3.75 ),
-        BoundingBox::new(-6.   , -5.   , 0. , 3. , 4.    , 5.    ),
-        BoundingBox::new(-3.5  , -2.5  , 0. , 3. , 4.    , 5.    ),
-        BoundingBox::new(1.5   , 2.5   , 0. , 3. , -4.75 , -3.75 ),
-        BoundingBox::new(1.5   , 2.5   , 0. , 3. , -3.   , -1.   ),
-        BoundingBox::new(-1.   , 0.0   , 0. , 3. , 4.    , 5.    ),
-        // BoundingBox::new(-1.0006559021421708, 0.9993443362764083, -1.255562663078308, 4.24443781375885, 0.9995791912078857, 2.999579429626465)
+        BoundingBox::new(-1., 1., 0., 2., -1., 1.),
+        BoundingBox::new(3., 5., 0., 2., -1., 1.),
+        BoundingBox::new(7., 9., 0., 2., -1., 1.),
+        BoundingBox::new(11., 13., 0., 2., -1., 1.),
+        BoundingBox::new(15., 17., 0., 2., -1., 1.),
     ];
 
     let world = World {
         camera,
         player: Player {
-            trf: Similarity3::new(Vec3::new(0.0, 2.0, 2.5), Rotor3::identity(), 1.),
+            trf: Similarity3::new(Vec3::new(0.0, 3.0, 0.0), Rotor3::identity(), 1.),
             model: player_model,
             a: Vec3::new(0., -0.03, 0.),
             v: Vec3::new(0., 0., 0.),
@@ -275,7 +225,7 @@ fn main() -> Result<()> {
         },
         level: Level {
             trf: Similarity3::new(Vec3::new(0.0, 0.0, 0.0), Rotor3::identity(), 1.),
-            model: level_model,
+            model: level_model.unwrap(),
         },
         bounding_boxes
     };
